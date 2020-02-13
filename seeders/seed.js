d265c35e9a134b6da40fbf04745eeffc3c3ff509
Jose Castro <jacastro023@gmail.com>
2020-02-12 23:20:27 -0600

    changed seed connection

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://user1:password1@ds147520.mlab.com:47520/heroku_lr7bf1fm", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
